41af36205064fb4ce63a5019e7f014445cde02d6
Darknov <darknov.dono@gmail.com>
2019-11-07 13:08:56 +0100

    Add some more juice to the app and skip empty question

--- js/app.js
+++ js/app.js
@@ -1,19 +1,57 @@
 import { getQuestions } from "./questions.js";
 import { updateView } from "./view.js";
 import { randomInt } from "./utils.js";
+import { addAnswerButtonEvent } from "./button.js";
+import { pageElements as elements } from "./pageElements.js";
 
 const app = {}
 
 app.state = {};
 
-app.nextQuestion = function () {
-  const randomQuestion = this.questions[randomInt(0, this.questions.length - 1)];
+app.addScore = function () {
+  this.state.correctAnswersNumber++;
+}
+
+app.addTotalAnswersNumber = function () {
+  this.state.totalAnswersNumber++;
+}
+
+app.checkAnswers = function () {
+
+}
+
+app.checkState = function () {
+  this.checkAnswers();
+}
+
+app.addAnswer = function (number) {
+  this.state.chosenAnswers.push(number);
+  this.checkState();
+}
+
+app.setButtons = function () {
+  for (let i = 0; i < elements.buttons.length; i++) {
+    buttons[i].addEventListener("click", () => {
+      app.addAnswer(i);
+    });
+  }
+}
+
+app.setQuestion = function () {
+  const randomQuestion = this.questions[randomInt(1, this.questions.length - 1)];
 
   this.state.correctAnswer = randomQuestion.correctAnswer;
   this.state.question = randomQuestion.question;
   this.state.possibleAnswers = randomQuestion.possibleAnswers;
   this.state.explanation = randomQuestion.explanation;
   this.state.showExplanation = true;
+  this.state.chosenAnswers = [];
+}
+
+app.nextQuestion = function () {
+  this.setQuestion();
+  updateView(this.state);
+
 }
 
 app.setup = async function () {
@@ -30,7 +68,6 @@ app.start = function () {
       this.nextQuestion();
       updateView(this.state);
     })
-
 }
 
 export { app };
